35c3afc246021797801f436093aaab5362b4fdca
ByungChangYoo <ryu.byungchang@gmail.com>
2017-12-06 12:39:09 +0900

    div.js 파일 추가 및 div 함수 호출 추가

---
+++ js/div.js
@@ -0,0 +1,7 @@
+function div() {
+	document.getElementById("div").onclick = function() {
+	  var num1 = parseInt(document.getElementById("num1").value);
+	  var num2 = parseInt(document.getElementById("num2").value);
+	  alert(num1 / num2);	  
+	};
+}
