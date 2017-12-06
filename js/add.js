1f7459bf455bb5955050bfc65d0c7bd0ff51f205
ByungChangYoo <ryu.byungchang@gmail.com>
2017-12-06 12:44:58 +0900

    js 삭제/html 재작성

--- js/add.js
+++
@@ -1,7 +0,0 @@
-function add() {
-	document.getElementById("add").onclick = function() {
-	  var num1 = parseInt(document.getElementById("num1").value);
-	  var num2 = parseInt(document.getElementById("num2").value);
-	  alert(num1 + num2);	  
-	};
-}
--- js/mul.js
+++
@@ -1,7 +0,0 @@
-function mul() {
-	document.getElementById("mul").onclick = function() {
-	  var num1 = parseInt(document.getElementById("num1").value);
-	  var num2 = parseInt(document.getElementById("num2").value);
-	  alert(num1 * num2);	  
-	};
-}
